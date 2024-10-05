5f2f350f8a5e5e3ab39a8a6acbdf4747b2bc1874
Abhishek <116814896+Intellect145@users.noreply.github.com>
2024-10-06 00:05:33 +0530

    Header Section has been updated and can downloaf the resume file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <NavBar/>
     </div>
   )
 }
